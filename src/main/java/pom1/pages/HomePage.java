551c6dfe008df89651764cc492bd3f3d2a4370f5
Aditi Sale <aditisale@Aditis-MacBook-Air.local>
2018-01-07 22:42:30 -0800

    HomePage Updated

--- src/main/java/pom1/pages/HomePage.java
+++ src/main/java/pom1/pages/HomePage.java
@@ -4,9 +4,13 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class HomePage {
 	protected static WebDriver driver;
+	protected static WebDriverWait wait;
+
+
 
 	public HomePage(){
         System.setProperty("webdriver.chrome.driver", "/Users/aditisale/Downloads/chromedriver");
@@ -14,6 +18,7 @@ public class HomePage {
         driver = new ChromeDriver();
         driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		wait = new WebDriverWait(driver, 2000);
         }
 }
 }
